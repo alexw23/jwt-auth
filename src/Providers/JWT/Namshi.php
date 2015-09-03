384b45f358b8f2c1c9f62b556a9f593121d42cf1
Alex Whiteside <alexwhiteside@ignitedlabs.com.au>
2015-09-03 17:13:12 +1000

    Added RS256 support

--- src/Providers/JWT/Namshi.php
+++ src/Providers/JWT/Namshi.php
@@ -29,9 +29,9 @@ class Namshi extends Provider implements JWT
      * @param string  $algo
      * @param null    $driver
      */
-    public function __construct($secret, $algo, $driver = null)
+    public function __construct($secret, $algo, $cert, $driver = null)
     {
-        parent::__construct($secret, $algo);
+        parent::__construct($secret, $algo, $cert);
 
         $this->jws = $driver ?: new JWS(['typ' => 'JWT', 'alg' => $algo]);
     }
@@ -72,7 +72,13 @@ class Namshi extends Provider implements JWT
             throw new TokenInvalidException('Could not decode token: ' . $e->getMessage());
         }
 
-        if (! $jws->verify($this->secret, $this->algo)) {
+        if (in_array($this->algo, ['RS256']) && $this->cert !== null) {
+            $key = $this->cert;
+        } else {
+            $key = $this->secret;
+        }
+
+        if (! $jws->verify($key, $this->algo)) {
             throw new TokenInvalidException('Token Signature could not be verified.');
         }
 
